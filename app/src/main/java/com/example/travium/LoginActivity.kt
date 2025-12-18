fb1fb57db2c32b571d63c623ce1b0d8a2d51d142
sonicgauchan10 <gauchansonic9972@gmail.com>
2025-12-18 09:21:22 +0545

    updated \login activity

--- app/src/main/java/com/example/travium/LoginActivity.kt
+++ app/src/main/java/com/example/travium/LoginActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -231,7 +230,6 @@ fun LoginBody() {
 //                context.startActivity(Intent(context, RegisterActivity::class.java))
                 fontSize = 18.sp
             )
-
         }
     }
 }
