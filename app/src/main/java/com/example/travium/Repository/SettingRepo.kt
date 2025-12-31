c740431eb13dea219dbc5924ab6ad4e94a5e4196
Wangeldona <wangelthokar@gmail.com>
2025-12-31 20:34:40 +0545

    Made ViewModel, Repo, repoimpl for Setting

--- app/src/main/java/com/example/travium/Repository/SettingRepo.kt
+++ app/src/main/java/com/example/travium/Repository/SettingRepo.kt
@@ -1,4 +1,16 @@
 package com.example.travium.Repository
 
-class SettingRepo {
+import com.example.travium.Model.SettingModel
+
+interface SettingRepo {
+    fun saveSettings(
+        userId: String,
+        settings: SettingModel,
+        callback: (Boolean, String) -> Unit
+    )
+
+    fun getSettings(
+        userId: String,
+        callback: (Boolean, String, SettingModel?) -> Unit
+    )
 }
