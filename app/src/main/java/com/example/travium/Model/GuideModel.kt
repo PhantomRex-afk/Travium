685030b11a5ee145b2d005a6ea5070f5ecad437d
PhantomRex-afk <superbshrestha7@gmail.com>
2026-01-27 15:47:26 +0545

    integrated map

--- app/src/main/java/com/example/travium/Model/GuideModel.kt
+++ app/src/main/java/com/example/travium/Model/GuideModel.kt
@@ -4,5 +4,14 @@ data class GuideModel(
     val guideId: String = "",
     val placeName: String = "",
     val imageUrls: List<String> = emptyList(),
-    val accommodations: String = ""
+    val accommodations: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val hotels: List<HotelLocation> = emptyList()
+)
+
+data class HotelLocation(
+    val name: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
 )
